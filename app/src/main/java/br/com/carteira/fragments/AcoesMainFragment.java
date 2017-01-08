9e89d000c9d7ca731306034ace63b75ce37a3469
thiagoprochnow <thiprochnow@gmail.com>
2017-01-08 17:27:24 -0200

    Created the Add New Stock DialogFragment
    Created the add new stock DialogFragment when clicking on the FAB
    button. Now when clicking on the FAB button, a DialogFragment will
    appear for the user to input the new stock information. Then if
    confirmed, the new stock will be shown in the portfolio, otherwise it
    will dismiss the Dialog and nothing will happen.

--- app/src/main/java/br/com/carteira/fragments/AcoesMainFragment.java
+++ app/src/main/java/br/com/carteira/fragments/AcoesMainFragment.java
@@ -1,12 +1,15 @@
 package br.com.carteira.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +26,7 @@ import br.com.carteira.domain.AcaoService;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AcoesMainFragment extends BaseFragment {
+public class AcoesMainFragment extends BaseFragment{
     List<Acao> acoes = new ArrayList<Acao>();
     protected RecyclerView recyclerView;
 
@@ -45,21 +48,7 @@ public class AcoesMainFragment extends BaseFragment {
         view.findViewById(R.id.fabAcoes).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                // For more information on each acao variable, check the Acao.java class
-                Acao acao = new Acao();
-                acao.ticker = "PETR4";
-                acao.stockQuantity = 100;
-                acao.boughtPrice = 23.45;
-                acao.boughtTotal = acao.stockQuantity * acao.boughtPrice;
-                acao.currentPrice = 35.50;
-                acao.currentTotal = acao.stockQuantity * acao.currentPrice;
-                acao.stockAppreciation = acao.currentTotal - acao.boughtTotal;
-                acao.targetPercent = 10.00;
-                acao.currentPercent = 20.00;
-                acao.totalIncome = 150.00;
-                acao.totalGain = acao.stockAppreciation + acao.totalIncome;
-                acoes.add(acao);
-                taskAcoes();
+                showAddAcaoDialog();
             }
         });
 
@@ -73,7 +62,7 @@ public class AcoesMainFragment extends BaseFragment {
     }
 
     private void taskAcoes(){
-        //Busca a lista de acoes
+        // Search for the stock list List<Acao>
         // If acoes list is empty, it will load from AcaoService, else it will add when clicked on FAB button
         if(acoes.size() == 0) {
             this.acoes = AcaoService.getAcoes(getContext());
@@ -83,11 +72,62 @@ public class AcoesMainFragment extends BaseFragment {
 
     private AcaoAdapter.AcaoOnClickListener onClickAcao(){
         return new AcaoAdapter.AcaoOnClickListener(){
+            // Implement the onClickAcao function from the interface of AcaoAdapter onClickListener
             @Override
             public void onClickAcao(View view, int idx){
                 Acao acao = acoes.get(idx);
                 Toast.makeText(getContext(), "Acao: " + acao.ticker, Toast.LENGTH_SHORT).show();
             }
         };
+    }
+
+    // Function to show the add stock dialog fragment
+    public void showAddAcaoDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new AddAcaoDialogFragment();
+        // Asks the new DialogFragment for a result with a Request_code = 0
+        dialog.setTargetFragment(this, 0);
+        dialog.show(getFragmentManager(), "AddAcaoDialogFragment");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        // Request code 0 is the return of the Dialog Fragment after filling the EditText field and pressing positive buttons
+        if(requestCode == 0){
+            String teste = intent.getStringExtra("codigoAcao");
+            // Add as a new stock to the portfolio or sums to already existing one.
+            boolean success = addStock(intent);
+            if(success){
+                Toast.makeText(getContext(), R.string.add_acao_success, Toast.LENGTH_SHORT);
+            } else {
+                Toast.makeText(getContext(), R.string.add_acao_fail, Toast.LENGTH_SHORT);
+            }
+        }
+    }
+
+    public boolean addStock(Intent intent) {
+        // Parse the information of the intent sent from the DialogFragment to add the stock
+        String inputTicker = intent.getStringExtra("inputTicker");
+        int inputQuantity = Integer.parseInt(intent.getStringExtra("inputQuantity"));
+        double inputBuyPrice = Double.parseDouble(intent.getStringExtra("inputBuyPrice"));
+        double inputObjective = Double.parseDouble(intent.getStringExtra("inputObjective"));
+
+        // For more information on each acao variable, check the Acao.java class
+        Acao acao = new Acao();
+        acao.ticker = inputTicker;
+        acao.stockQuantity = inputQuantity;
+        acao.boughtPrice = inputBuyPrice;
+        acao.boughtTotal = acao.stockQuantity * acao.boughtPrice;
+        acao.currentPrice = 35.50;
+        acao.currentTotal = acao.stockQuantity * acao.currentPrice;
+        acao.stockAppreciation = acao.currentTotal - acao.boughtTotal;
+        acao.targetPercent = inputObjective;
+        acao.currentPercent = 20.00;
+        acao.totalIncome = 150.00;
+        acao.totalGain = acao.stockAppreciation + acao.totalIncome;
+        acoes.add(acao);
+        taskAcoes();
+        return true;
     }
 }
