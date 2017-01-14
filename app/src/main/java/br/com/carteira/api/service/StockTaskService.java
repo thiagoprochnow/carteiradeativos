42ed833591b4c88e0426f9233a20196c7e27b458
Walter <jnunes.walter@gmail.com>
2017-01-13 23:19:43 -0200

    First functional implementation of the Service that is responsible to make the requests to the API.

---
+++ app/src/main/java/br/com/carteira/api/service/StockTaskService.java
@@ -0,0 +1,78 @@
+package br.com.carteira.api.service;
+
+import android.util.Log;
+
+import java.io.IOException;
+
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.GcmTaskService;
+import com.google.android.gms.gcm.TaskParams;
+
+import br.com.carteira.api.domain.ResponseStock;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Task Service responsible for making the Api request and parser the result.
+ * This class will also write the result in the database and update the adapter
+ */
+public class StockTaskService extends GcmTaskService {
+
+    // Log variable
+    private String LOG_TAG = StockTaskService.class.getSimpleName();
+
+    @Override
+    public int onRunTask(TaskParams params) {
+
+        int resultStatus = GcmNetworkManager.RESULT_FAILURE;
+
+        // Build retrofit base request
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(StockService.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        try {
+
+            // Validate if Symbol was added
+            if (params.getExtras().getString(StockIntentService.ADD_SYMBOL).isEmpty()) {
+                throw new IOException("Missing Extra ADD_SYMBOL");
+            }
+
+            // Prepare the query to be added in YQL (Yahoo API)
+            String query = "select * from yahoo.finance.quotes where symbol in (\""
+                    + params.getExtras().getString(StockIntentService.ADD_SYMBOL) + "\")";
+
+
+            // Make the request and parse the result
+            StockService service = retrofit.create(StockService.class);
+            Call<ResponseStock> call = service.getStock(query);
+            Response<ResponseStock> response = call.execute();
+            ResponseStock responseGetStock = response.body();
+
+            // Debug test log - the return will be changed for multiple symbols
+            if(responseGetStock.getStockQuotes() != null) {
+                Log.d(LOG_TAG, "Response log test:" +
+                        "\nquery: " + query +
+                        "\nsymbol: " + responseGetStock.getStockQuotes().get(0).getSymbol() +
+                        "\nname: " + responseGetStock.getStockQuotes().get(0).getName() +
+                        "\nbid: " + responseGetStock.getStockQuotes().get(0).getBid() +
+                        "\nchange: " + responseGetStock.getStockQuotes().get(0).getChange() +
+                        "\nchange in percent: " + responseGetStock.getStockQuotes().get(0).getChangeInPercent() +
+                        "\nday low: " + responseGetStock.getStockQuotes().get(0).getDaysLow() +
+                        "\nday high: " + responseGetStock.getStockQuotes().get(0).getDaysHigh() +
+                        "");
+            }
+
+            // Success request
+            resultStatus = GcmNetworkManager.RESULT_SUCCESS;
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error in request " + e.getMessage());
+            e.printStackTrace();
+        }
+        return resultStatus;
+    }
+
+}
--- app/src/main/java/br/com/carteira/fragments/EstatisticasMainFragment.java
+++ app/src/main/java/br/com/carteira/fragments/EstatisticasMainFragment.java
@@ -1,6 +1,7 @@
 package br.com.carteira.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import br.com.carteira.R;
+import br.com.carteira.api.service.StockIntentService;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +26,12 @@ public class EstatisticasMainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        // Run the initialize task service so that some stocks appear upon an empty database
+        Intent mServiceIntent = new Intent(getActivity(), StockIntentService.class);
+        mServiceIntent.putExtra(StockIntentService.ADD_SYMBOL, "YHOO");
+
+        getActivity().startService(mServiceIntent);
         return inflater.inflate(R.layout.fragment_estatisticas_main, container, false);
     }
 
