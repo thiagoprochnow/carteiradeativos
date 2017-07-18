d4d340b7ef93398965163cf2534c194ceaa15fb2
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2017-07-17 21:27:20 -0300

    Development of pie chart
    Development of pie chart:
    - Currency
    - Fii
    - Fixed Income
    -Treasury

--- app/src/main/java/br/com/carteira/adapter/fii/FiiOverviewAdapter.java
+++ app/src/main/java/br/com/carteira/adapter/fii/FiiOverviewAdapter.java
@@ -2,19 +2,39 @@ package br.com.carteira.adapter.fii;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import br.com.carteira.R;
 import br.com.carteira.data.PortfolioContract;
+import br.com.carteira.utils.MyPercentFormatter;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -47,10 +67,8 @@ public class FiiOverviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
                 item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fii_overview, parent, false);
                 return new FiiOverviewViewHolder(item);
             default:
-                item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fii_overview, parent, false);
-                return new FiiOverviewViewHolder(item);
-                //item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fii, parent, false);
-                //return new FiiDataViewHolder(item);
+                item = LayoutInflater.from(mContext).inflate(R.layout.adapter_piechart, parent, false);
+                return new FiiPieChartViewHolder(item);
         }
     }
 
@@ -116,8 +134,66 @@ public class FiiOverviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
                 viewHolder.totalGainPercent.setText("(" + String.format("%.2f", totalGainPercent) + "%)");
                 break;
             default:
-                // If it is one of the FiiData adapter views
-                Log.d(LOG_TAG, "No ViewHolder found");
+                FiiOverviewAdapter.FiiPieChartViewHolder chartHolder = (FiiOverviewAdapter.FiiPieChartViewHolder) holder;
+
+                List<PieEntry> entries = new ArrayList<>();
+                Cursor dataCursor = getDataCursor();
+
+                if(dataCursor.getCount() > 0) {
+                    dataCursor.moveToFirst();
+                    float otherPercent = 0;
+                    do{
+                        float currentPercent = dataCursor.getFloat(0);
+                        String symbol = dataCursor.getString(1);
+                        // Check if already reach others field
+                        // Show each pie data order in asc form
+                        // Do not show sold fii
+                        if (currentPercent > 0) {
+                            if (dataCursor.getPosition() < 8) {
+                                entries.add(new PieEntry(currentPercent, symbol));
+                            } else {
+                                // Check if is last fii data
+                                otherPercent += currentPercent;
+                                if (dataCursor.getPosition() == dataCursor.getCount() - 1) {
+                                    entries.add(new PieEntry(otherPercent, mContext.getResources().getString(R.string.portfolio_other_label)));
+                                }
+                            }
+                        }
+                    } while (dataCursor.moveToNext());
+
+                    // Animation on show
+                    chartHolder.pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+                    chartHolder.pieChart.setDrawHoleEnabled(true);
+                    chartHolder.pieChart.setHoleColor(mContext.getResources().getColor(R.color.activity_main_background));
+                    PieDataSet dataSet = new PieDataSet(entries, "");
+                    chartHolder.pieChart.setHoleRadius(58f);
+                    chartHolder.pieChart.setTransparentCircleRadius(61f);
+                    chartHolder.pieChart.setOnChartValueSelectedListener(chartHolder);
+
+                    dataSet.setColors(new int[]{R.color.green, R.color.blue, R.color.red,
+                            R.color.yellow, R.color.darkBlue, R.color.lightGray, R.color.wine, R.color.darkGreen, R.color.darkGray}, mContext);
+
+                    Legend l = chartHolder.pieChart.getLegend();
+                    l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_CENTER);
+                    l.setXEntrySpace(7f);
+                    l.setYEntrySpace(5f);
+
+                    PieData data = new PieData(dataSet);
+                    chartHolder.pieChart.setData(data);
+                    data.setValueTextSize(12f);
+                    data.setValueTextColor(Color.BLACK);
+                    // Set as Percent
+                    data.setValueFormatter(new MyPercentFormatter());
+                    //Hides labels
+                    chartHolder.pieChart.setDrawEntryLabels(false);
+                    // Hide Description
+                    chartHolder.pieChart.setDescription(null);
+                    chartHolder.pieChart.invalidate(); // refresh
+
+                    Log.d(LOG_TAG, "Pie Chart Drawn");
+                } else {
+                    Log.d(LOG_TAG, "No FiiData found for pieChart");
+                }
         }
 
     }
@@ -125,8 +201,9 @@ public class FiiOverviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     @Override
     public int getItemCount() {
         int count = 0;
-        if (mCursor != null) {
+        if (mCursor != null && mCursor.getCount() > 0) {
             count = mCursor.getCount();
+            count++;
         }
         return count;
     }
@@ -164,5 +241,55 @@ public class FiiOverviewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
+    }
+
+    class FiiPieChartViewHolder extends RecyclerView.ViewHolder implements OnChartValueSelectedListener {
+
+        @BindView(R.id.chart)
+        PieChart pieChart;
+
+        public FiiPieChartViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public void onValueSelected(Entry e, Highlight h) {
+
+            if (e == null)
+                return;
+            PieEntry pe = (PieEntry) e;
+            Log.d("VAL SELECTED",
+                    "Value: " + e.getY() + ", Label: " + pe.getLabel());
+            pieChart.setCenterText(generateCenterSpannableText(pe.getLabel()));
+        }
+
+        @Override
+        public void onNothingSelected() {
+            Log.d("PieChart", "nothing selected");
+        }
+
+        private SpannableString generateCenterSpannableText(String text) {
+
+            SpannableString s = new SpannableString(text);
+            s.setSpan(new StyleSpan(Typeface.NORMAL), 0, s.length(), 0);
+            s.setSpan(new ForegroundColorSpan(Color.GRAY), 0, s.length(), 0);
+            s.setSpan(new RelativeSizeSpan(2f), 0, s.length(), 0);
+            s.setSpan(new StyleSpan(Typeface.ITALIC), s.length(), s.length(), 0);
+            s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length(), s.length(), 0);
+            return s;
+        }
+    }
+
+    private Cursor getDataCursor(){
+        String[] affectedColumn = {PortfolioContract.FiiData.COLUMN_CURRENT_PERCENT,
+                PortfolioContract.FiiData.COLUMN_SYMBOL};
+        String sortOrder = PortfolioContract.FiiData.COLUMN_CURRENT_PERCENT + " DESC";
+
+        // Searches for existing FiiData to update value.
+        // If dosent exists, creates new one
+        return mContext.getContentResolver().query(
+                PortfolioContract.FiiData.URI,
+                affectedColumn, null, null, sortOrder);
     }
 }
--- app/src/main/java/br/com/carteira/adapter/fixedincome/FixedOverviewAdapter.java
+++ app/src/main/java/br/com/carteira/adapter/fixedincome/FixedOverviewAdapter.java
@@ -2,19 +2,39 @@ package br.com.carteira.adapter.fixedincome;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import br.com.carteira.R;
 import br.com.carteira.data.PortfolioContract;
+import br.com.carteira.utils.MyPercentFormatter;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -47,10 +67,8 @@ public class FixedOverviewAdapter extends RecyclerView.Adapter<RecyclerView.View
                 item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fixed_overview, parent, false);
                 return new FixedOverviewViewHolder(item);
             default:
-                item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fixed_overview, parent, false);
-                return new FixedOverviewViewHolder(item);
-                //item = LayoutInflater.from(mContext).inflate(R.layout.adapter_fixed, parent, false);
-                //return new FixedDataViewHolder(item);
+                item = LayoutInflater.from(mContext).inflate(R.layout.adapter_piechart, parent, false);
+                return new FixedPieChartViewHolder(item);
         }
     }
 
@@ -90,8 +108,66 @@ public class FixedOverviewAdapter extends RecyclerView.Adapter<RecyclerView.View
                 viewHolder.totalGainPercent.setText("(" + String.format("%.2f", totalGainPercent) + "%)");
                 break;
             default:
-                // If it is one of the FixedData adapter views
-                Log.d(LOG_TAG, "No ViewHolder found");
+                FixedOverviewAdapter.FixedPieChartViewHolder chartHolder = (FixedOverviewAdapter.FixedPieChartViewHolder) holder;
+
+                List<PieEntry> entries = new ArrayList<>();
+                Cursor dataCursor = getDataCursor();
+
+                if(dataCursor.getCount() > 0) {
+                    dataCursor.moveToFirst();
+                    float otherPercent = 0;
+                    do{
+                        float currentPercent = dataCursor.getFloat(0);
+                        String symbol = dataCursor.getString(1);
+                        // Check if already reach others field
+                        // Show each pie data order in asc form
+                        // Do not show sold fixed
+                        if (currentPercent > 0) {
+                            if (dataCursor.getPosition() < 8) {
+                                entries.add(new PieEntry(currentPercent, symbol));
+                            } else {
+                                // Check if is last fixed data
+                                otherPercent += currentPercent;
+                                if (dataCursor.getPosition() == dataCursor.getCount() - 1) {
+                                    entries.add(new PieEntry(otherPercent, mContext.getResources().getString(R.string.portfolio_other_label)));
+                                }
+                            }
+                        }
+                    } while (dataCursor.moveToNext());
+
+                    // Animation on show
+                    chartHolder.pieChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+                    chartHolder.pieChart.setDrawHoleEnabled(true);
+                    chartHolder.pieChart.setHoleColor(mContext.getResources().getColor(R.color.activity_main_background));
+                    PieDataSet dataSet = new PieDataSet(entries, "");
+                    chartHolder.pieChart.setHoleRadius(58f);
+                    chartHolder.pieChart.setTransparentCircleRadius(61f);
+                    chartHolder.pieChart.setOnChartValueSelectedListener(chartHolder);
+
+                    dataSet.setColors(new int[]{R.color.green, R.color.blue, R.color.red,
+                            R.color.yellow, R.color.darkBlue, R.color.lightGray, R.color.wine, R.color.darkGreen, R.color.darkGray}, mContext);
+
+                    Legend l = chartHolder.pieChart.getLegend();
+                    l.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_CENTER);
+                    l.setXEntrySpace(7f);
+                    l.setYEntrySpace(5f);
+
+                    PieData data = new PieData(dataSet);
+                    chartHolder.pieChart.setData(data);
+                    data.setValueTextSize(12f);
+                    data.setValueTextColor(Color.BLACK);
+                    // Set as Percent
+                    data.setValueFormatter(new MyPercentFormatter());
+                    //Hides labels
+                    chartHolder.pieChart.setDrawEntryLabels(false);
+                    // Hide Description
+                    chartHolder.pieChart.setDescription(null);
+                    chartHolder.pieChart.invalidate(); // refresh
+
+                    Log.d(LOG_TAG, "Pie Chart Drawn");
+                } else {
+                    Log.d(LOG_TAG, "No FixedData found for pieChart");
+                }
         }
 
     }
@@ -99,8 +175,9 @@ public class FixedOverviewAdapter extends RecyclerView.Adapter<RecyclerView.View
     @Override
     public int getItemCount() {
         int count = 0;
-        if (mCursor != null) {
+        if (mCursor != null && mCursor.getCount() > 0) {
             count = mCursor.getCount();
+            count++;
         }
         return count;
     }
@@ -126,5 +203,55 @@ public class FixedOverviewAdapter extends RecyclerView.Adapter<RecyclerView.View
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
+    }
+
+    class FixedPieChartViewHolder extends RecyclerView.ViewHolder implements OnChartValueSelectedListener {
+
+        @BindView(R.id.chart)
+        PieChart pieChart;
+
+        public FixedPieChartViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public void onValueSelected(Entry e, Highlight h) {
+
+            if (e == null)
+                return;
+            PieEntry pe = (PieEntry) e;
+            Log.d("VAL SELECTED",
+                    "Value: " + e.getY() + ", Label: " + pe.getLabel());
+            pieChart.setCenterText(generateCenterSpannableText(pe.getLabel()));
+        }
+
+        @Override
+        public void onNothingSelected() {
+            Log.d("PieChart", "nothing selected");
+        }
+
+        private SpannableString generateCenterSpannableText(String text) {
+
+            SpannableString s = new SpannableString(text);
+            s.setSpan(new StyleSpan(Typeface.NORMAL), 0, s.length(), 0);
+            s.setSpan(new ForegroundColorSpan(Color.GRAY), 0, s.length(), 0);
+            s.setSpan(new RelativeSizeSpan(2f), 0, s.length(), 0);
+            s.setSpan(new StyleSpan(Typeface.ITALIC), s.length(), s.length(), 0);
+            s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length(), s.length(), 0);
+            return s;
+        }
+    }
+
+    private Cursor getDataCursor(){
+        String[] affectedColumn = {PortfolioContract.FixedData.COLUMN_CURRENT_PERCENT,
+                PortfolioContract.FixedData.COLUMN_SYMBOL};
+        String sortOrder = PortfolioContract.FixedData.COLUMN_CURRENT_PERCENT + " DESC";
+
+        // Searches for existing FixedData to update value.
+        // If dosent exists, creates new one
+        return mContext.getContentResolver().query(
+                PortfolioContract.FixedData.URI,
+                affectedColumn, null, null, sortOrder);
     }
 }
