af2d4d2a03be872be1ec76105908ae049f2c87d3
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2017-09-01 21:09:12 -0300

    Minor changes
    Minor changes

--- app/src/main/java/br/com/guiainvestimento/adapter/currency/CurrencyOverviewAdapter.java
+++ app/src/main/java/br/com/guiainvestimento/adapter/currency/CurrencyOverviewAdapter.java
@@ -242,7 +242,7 @@ public class CurrencyOverviewAdapter extends RecyclerView.Adapter<RecyclerView.V
             SpannableString s = new SpannableString(text+"\n"+valueS);
             s.setSpan(new StyleSpan(Typeface.NORMAL), 0, s.length(), 0);
             s.setSpan(new ForegroundColorSpan(Color.GRAY), 0, s.length(), 0);
-            s.setSpan(new RelativeSizeSpan(1f), 0, s.length(), 0);
+            s.setSpan(new RelativeSizeSpan(1.4f), 0, s.length(), 0);
             s.setSpan(new StyleSpan(Typeface.ITALIC), s.length(), s.length(), 0);
             s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length(), s.length(), 0);
             return s;
