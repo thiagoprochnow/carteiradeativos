95a71fe5fcaef9c48a2960b47c14864cde629aaf
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2017-09-27 21:02:23 -0300

    Added Contact email and app version informations
    Added Contact email and app version informations

--- app/src/main/java/br/com/guiainvestimento/fragment/ComingSoonFragment.java
+++ app/src/main/java/br/com/guiainvestimento/fragment/ComingSoonFragment.java
@@ -2,11 +2,13 @@ package br.com.guiainvestimento.fragment;
 
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import br.com.guiainvestimento.R;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ComingSoonFragment extends BaseFragment{
