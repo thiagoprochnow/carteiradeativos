2dadcdd7e7d27c51a42aead3ad465d6380c2eff2
Thiago Henrique Prochnow <thiprochnow@gmail.com>
2018-05-01 20:54:59 -0300

    Change way of inserting incomes to make it easier
    Change way of inserting incomes to make it easier
    Added Floating action button

--- app/src/main/java/br/com/guiainvestimento/activity/ProductDetailsActivity.java
+++ app/src/main/java/br/com/guiainvestimento/activity/ProductDetailsActivity.java
@@ -1,12 +1,22 @@
 package br.com.guiainvestimento.activity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import br.com.guiainvestimento.R;
 import br.com.guiainvestimento.common.Constants;
@@ -19,8 +29,12 @@ import br.com.guiainvestimento.fragment.treasury.TreasuryTabFragment;
 import br.com.guiainvestimento.listener.ProductListener;
 import br.com.guiainvestimento.listener.IncomeDetailsListener;
 import br.com.guiainvestimento.listener.TransactionListener;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ProductDetailsActivity extends AppCompatActivity implements IncomeDetailsListener, TransactionListener {
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
 
     private ProductListener mFormProductListener;
 
@@ -30,11 +44,16 @@ public class ProductDetailsActivity extends AppCompatActivity implements IncomeD
 
     int mProductType;
 
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        mContext = this;
+        ButterKnife.bind(this);
         comingIntent = getIntent();
+        registerForContextMenu(fab);
         if (comingIntent != null && comingIntent.hasExtra(Constants.Extra.EXTRA_PRODUCT_TYPE)) {
 
             mProductType = comingIntent.getIntExtra(Constants.Extra.EXTRA_PRODUCT_TYPE,
@@ -63,6 +82,8 @@ public class ProductDetailsActivity extends AppCompatActivity implements IncomeD
                     finish();
                     break;
             }
+
+            fab.setOnClickListener(onClickListener());
         } else {
         }
     }
@@ -92,6 +113,36 @@ public class ProductDetailsActivity extends AppCompatActivity implements IncomeD
             default:
                 return true;
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        // Common menu for details
+        switch (mProductType) {
+            case Constants.ProductType.STOCK:
+                getMenuInflater().inflate(R.menu.stock_details_menu, menu);
+                break;
+            case Constants.ProductType.FII:
+                getMenuInflater().inflate(R.menu.fii_details_menu, menu);
+                break;
+            case Constants.ProductType.TREASURY:
+                getMenuInflater().inflate(R.menu.treasury_details_menu, menu);
+                break;
+            case Constants.ProductType.OTHERS:
+                getMenuInflater().inflate(R.menu.others_details_menu, menu);
+                break;
+            default:
+        }
+    }
+
+    public FloatingActionButton.OnClickListener onClickListener(){
+        FloatingActionButton.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openContextMenu(fab);
+            }
+        };
+        return listener;
     }
 
     @Override
@@ -135,6 +186,51 @@ public class ProductDetailsActivity extends AppCompatActivity implements IncomeD
                 break;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, FormActivity.class);
+        String symbol = comingIntent.getStringExtra(Constants.Extra.EXTRA_PRODUCT_SYMBOL);
+        intent.putExtra(Constants.Extra.EXTRA_PRODUCT_SYMBOL, symbol);
+        // Open correct income form
+        switch (item.getItemId()) {
+            case R.id.menu_item_dividends:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.DIVIDEND);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_jcp:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.JCP);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_bonification:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.BONIFICATION);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_split:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.SPLIT);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_grouping:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.GROUPING);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_income:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.FII);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_treasury_income:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.TREASURY);
+                startActivity(intent);
+                break;
+            case R.id.menu_item_others_income:
+                intent.putExtra(Constants.Extra.EXTRA_INCOME_TYPE, Constants.IncomeType.OTHERS);
+                startActivity(intent);
+                break;
+            default:
+                return super.onContextItemSelected(item);
         }
         return true;
     }
